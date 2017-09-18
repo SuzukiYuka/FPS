860ac0d7e46e39bdb7ea1e13a192cd801aaf046a
Yuka Suzuki <suzukiyuka1218@gmail.com>
2017-09-18 17:58:03 +0900

    finish text No19

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyScript : MonoBehaviour {
+
+    public Transform target;
+    NavMeshAgent agent;
+
+    int enemyHP = 3;
+
+    void Start () {
+        GameObject player = GameObject.Find("FPSController");
+        target = player.transform;
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        agent.SetDestination(target.position);
+	}
+
+    void Damage() {
+        enemyHP -= 1;
+        if (enemyHP == 0) {
+			GameObject player = GameObject.Find("FPSController");
+            player.SendMessage("DefeatEnemy");
+			Destroy(this.gameObject);
+        }
+    }
+	private void OnTriggerEnter(Collider other) {
+		if (other.gameObject.name == "FPSController") {
+            other.SendMessage("PlayerDamage");
+            Destroy(this.gameObject);
+        }
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour {
     public Camera mainCamera;
-    public Text score;
-    private int count;
+    public AudioSource gunSound;
+    int playerHP = 3;
+	public Text score;
+    public Text playerHPText;
+	private int count;
 
 	// Use this for initialization
 	void Start () {
@@ -18,25 +22,40 @@ public class PlayerController : MonoBehaviour {
 	void Update () {
         if (Input.GetMouseButtonDown(0)){
 			Shot();
+            gunSound.Play();
 		}
             
     }
+    void PlayerDamage() {
+        playerHP -= 1;
+        playerHPText.text = playerHP.ToString();
+        if (playerHP == 0) {
+            SceneManager.LoadScene("GameOver");
+        }
 
+    }
     void Shot() {
         int distance = 50;
         Vector3 center = new Vector3(Screen.width / 2, Screen.height / 2, 0);
         Ray ray = mainCamera.ScreenPointToRay(center);
         RaycastHit hitInfo;
 
-        if (Physics.Raycast(ray,out hitInfo, distance)) {
+        if (Physics.Raycast(ray, out hitInfo, distance))
+        {
 
-            if (hitInfo.collider.tag == "Enemy") {
-                count += 1;
-                score.text = count.ToString();
-                Renderer _renderer = hitInfo.collider.GetComponent<Renderer>();
-                _renderer.material.color = new Color(1f, 0, 0, 1f);
+            if (hitInfo.collider.tag == "Enemy")
+            {
+
+                //Renderer _renderer = hitInfo.collider.GetComponent<Renderer>();
+                //_renderer.material.color = new Color(1f, 0, 0, 1f);
+                hitInfo.collider.SendMessage("Damage");
 
             }
         }
+    }
+
+    void DefeatEnemy() {
+		count += 1;
+		score.text = count.ToString();
     }
 }
